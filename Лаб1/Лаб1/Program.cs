75394fccfd821079f4373a3569d5523348ffe1a0
timofei <rambambu79@gmail.com>
2023-03-29 12:12:52 +0300

    Console.WriteLine("Hello!");

--- Лаб1/Лаб1/Program.cs
+++ Лаб1/Лаб1/Program.cs
@@ -6,7 +6,7 @@ namespace Лаб1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
